2109060103c8e201a2583a6a9ec57626cf50204b
seryio <siyito.lpgc@gmail.com>
2020-04-10 18:01:56 +0200

    Readme updated

--- AUX_run-all.sh
+++ AUX_run-all.sh
@@ -1,7 +1,11 @@
 ./1_create_vnet.sh
 ./2.1_create_aks_master_vms.sh
-./3.1_create_aks_workers_vms.sh
 ./2.2_create_aks_master_lb.sh
+./3.1_create_aks_workers_vms.sh
+./4.10_copy_certs_masters.sh
+./4.11_create_aks_config.sh
+./4.12_copy_aks_config.sh
+./4.13_copy_aks_master_encryption.sh
 ./4.1_create_ca.sh
 ./4.2_create_cert_admin.sh
 ./4.3_create_cert_workers.sh
@@ -11,13 +15,10 @@
 ./4.7_create_cert_api.sh
 ./4.8_create_cert_service.sh
 ./4.9_copy_certs_workers.sh
-./4.10_copy_certs_masters.sh
-./4.11_create_aks_config.sh
-./4.12_copy_aks_config.sh
-./4.13_copy_aks_master_encryption.sh
 ./5.1_create_aks_master_etcd.sh
 ./5.2_create_aks_master_control.sh
 ./6.1_create_aks_workers.sh
 ./7.1_generate_kubectl_config.sh
 ./8.1_generate_aks_worker_routes.sh
 ./9.1_aks_dns_addon.sh
+./9.2_check_pod.sh
